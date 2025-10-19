a664db7c3086a849b4be6720885d6c3fce483f66
Alejandro <71718967+abeguerr01@users.noreply.github.com>
2025-10-19 19:07:25 +0200

    Add files via upload

---
+++ src/main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author USER
+ */
+public class main extends JFrame{
+    public static void main(String[] args) {
+        
+        Menu m = new Menu();
+        m.setVisible(true);
+        
+        System.out.println("Aplicacion funcionando correctamente");
+    }
+    
+}

f80b1c3f09af2651043de7bf33a57a9defca8754
Alejandro <alejandro.beguer@gmail.com>
2025-10-24 13:45:13 +0200

    subida a maven

---
+++ src/main/java/com/mycompany/di_p3_maven/DI_p3_maven.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.di_p3_maven;
+
+/**
+ *
+ * @author USER
+ */
+public class DI_p3_maven {
+
+     public static void main(String[] args) {
+        
+        Menu m = new Menu();
+        m.setVisible(true);
+        
+        System.out.println("Aplicacion funcionando correctamente");
+    }
+    
+}
---
+++ src/main/java/com/mycompany/di_p3_maven/FondoPanel.java
@@ -0,0 +1,34 @@
+package com.mycompany.di_p3_maven;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class FondoPanel extends JPanel {
+    
+    private Image imagenDeFondo;
+    
+        private final String RUTA_IMAGEN = "img/fondoMenu.jpg";
+
+    public FondoPanel() {
+        try {
+        imagenDeFondo = ImageIO.read(
+            getClass().getClassLoader().getResourceAsStream(RUTA_IMAGEN)
+        );
+        }catch(IOException e) {
+            System.err.println("Error al cargar la imagen de fondo: " + RUTA_IMAGEN);
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g); 
+        
+        if (imagenDeFondo != null) {
+            g.drawImage(imagenDeFondo, 0, 0, getWidth(), getHeight(), this);
+        }
+    }
+}
